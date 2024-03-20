1a508fe0d1ceb14ff2e9096aa34b2e4ea863d9ee
pradeep <p.guta@gmail.com>
2024-03-20 12:15:09 +0530

    vpc terraform

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "peering" {
   peer_owner_id = var.account_no
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
