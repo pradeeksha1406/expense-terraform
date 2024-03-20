b6ab3003f6f6f27e5fa7ec0309f2c219041d6246
pradeep <p.guta@gmail.com>
2024-03-20 12:29:52 +0530

    vpc terraform

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ module "vpc" {
   private_subnets = var.private_subnets
   public_subnets = var.public_subnets
   azs = var.azs
-  account_no = account_no
-  default_vpc_id = default_vpc_id
+  account_no = var.account_no
+  default_vpc_id = var.default_vpc_id
 }
