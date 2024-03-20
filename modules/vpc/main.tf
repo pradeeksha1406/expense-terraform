fda9f29ec6fd8bc552b491952cad767c180b510b
pradeep <p.guta@gmail.com>
2024-03-20 12:16:10 +0530

    vpc terraform

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -11,77 +11,78 @@ resource "aws_vpc_peering_connection" "peering" {
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
   auto_accept   = true
-  tags = {
-    Name= "perring-from-default-vpc-to-${var.env}-vpc"
-}
-
-resource "aws_subnet" "public_subnets" {
-  count = length(var.public_subnets)
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.public_subnets[count.index]
-  availability_zone = var.azs[count.index]
-  tags = {
-    Name = "public-subnet-${count.index+1}"
-
-  }
-}
-
-resource "aws_subnet" "private_subnets" {
-  count = length(var.private_subnets)
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.private_subnets[count.index]
-  availability_zone = var.azs[count.index]
-  tags = {
-    Name = "private-subnet-${count.index+1}"
-
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${var.env}-igw"
-  }
-}
-
-resource "aws_eip" "ngw" {
-    domain   = "vpc"
-
-}
-
-resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.ngw.id
-  subnet_id     = aws_subnet.public_subnets[0].id
-
-  tags = {
-    Name = "${var.env}-ngw"
+  tags          = {
+    Name = "perring-from-default-vpc-to-${var.env}-vpc"
   }
 
-}
+  resource "aws_subnet" "public_subnets" {
+    count             = length(var.public_subnets)
+    vpc_id            = aws_vpc.main.id
+    cidr_block        = var.public_subnets[count.index]
+    availability_zone = var.azs[count.index]
+    tags              = {
+      Name = "public-subnet-${count.index+1}"
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    }
   }
 
-  tags = {
-    Name = "public"
-  }
-}
+  resource "aws_subnet" "private_subnets" {
+    count             = length(var.private_subnets)
+    vpc_id            = aws_vpc.main.id
+    cidr_block        = var.private_subnets[count.index]
+    availability_zone = var.azs[count.index]
+    tags              = {
+      Name = "private-subnet-${count.index+1}"
 
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "10.0.1.0/24"
-    nat_gateway_id = aws_nat_gateway.ngw.id
+    }
   }
 
-  tags = {
-    Name = "private"
+  resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+      Name = "${var.env}-igw"
+    }
+  }
+
+  resource "aws_eip" "ngw" {
+    domain = "vpc"
+
+  }
+
+  resource "aws_nat_gateway" "ngw" {
+    allocation_id = aws_eip.ngw.id
+    subnet_id     = aws_subnet.public_subnets[0].id
+
+    tags = {
+      Name = "${var.env}-ngw"
+    }
+
+  }
+
+  resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.igw.id
+    }
+
+    tags = {
+      Name = "public"
+    }
+  }
+
+  resource "aws_route_table" "private" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+      cidr_block     = "10.0.1.0/24"
+      nat_gateway_id = aws_nat_gateway.ngw.id
+    }
+
+    tags = {
+      Name = "private"
+    }
   }
 }
