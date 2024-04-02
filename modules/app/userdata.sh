6e882997d19b0a743cf2b90715e0a97da08268d3
pradeep <p.guta@gmail.com>
2024-04-02 15:35:24 +0530

    vpc terraform

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo yum install python3.11-pip.noarch -y
-Sudo pip3.11 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/pradeeksha1406/infra-ansible.git main.yml -e roles_name=$ {roles_name}
+sudo yum install python3.11-pip ansible -y | tee -a /opt/userdata.log
+Sudo pip3.11 install botocore boto3 | tee -a /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/pradeeksha1406/infra-ansible.git main.yml -e roles_name=${roles_name} | tee -a /opt/userdata.log
