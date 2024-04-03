e6b795395e4798c78331b690932c08834567fa50
pradeep <p.guta@gmail.com>
2024-04-03 10:51:02 +0530

    vpc terraform

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "security_group" {
   #    protocol    = "tcp"
   #    cidr_blocks = [var.alb_sg_allow_cidr]
   #  }
-  #
+
     egress {
       from_port   = 0
       to_port     = 0
@@ -39,7 +39,50 @@ resource "aws_security_group" "security_group" {
   }
 }
 
+resource "aws_iam_role" "role" {
+  name = "${var.env}-${var.component}-role"
 
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "${var.env}-${var.component}-policy"
+
+    policy = jsonencode({
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "VisualEditor0",
+          "Effect" : "Allow",
+          "Action" : [
+            "kms:Decrypt",
+            "ssm:DescribeParameters",
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource" : "*"
+        }
+      ]
+    })
+
+  tags = {
+    tag-key = "${var.env}-${var.component}-role"
+  }
+}
 
 resource "aws_launch_template" "template" {
   name                   = "${var.env}-${var.component}"
