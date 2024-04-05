114675763c8708e73007083874cf077a95f72753
pradeep <p.guta@gmail.com>
2024-04-05 10:09:36 +0530

    vpc terraform

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -18,13 +18,13 @@ resource "aws_security_group" "security_group" {
     protocol    = "tcp"
     cidr_blocks = [var.alb_sg_allow_cidr]
   }
-#
-#  egress {
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "${var.env}-${var.alb_type}-sg"
