3ac2db5354c287bfff7a719511a1fab920095071
pradeep <p.guta@gmail.com>
2024-04-05 10:02:33 +0530

    vpc terraform

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -11,13 +11,13 @@ resource "aws_security_group" "security_group" {
     cidr_blocks = [var.alb_sg_allow_cidr]
   }
 
-#  ingress {
-#    description = "HTTPS"
-#    from_port   = 443
-#    to_port     = 443
-#    protocol    = "tcp"
-#    cidr_blocks = [var.alb_sg_allow_cidr]
-#  }
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.alb_sg_allow_cidr]
+  }
 #
 #  egress {
 #    from_port   = 0
